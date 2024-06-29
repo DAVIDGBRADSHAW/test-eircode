df2bf7aa55d9b17c86ead7cdb56d6e14df9e02c0
DAVIDGBRADSHAW <DAVIDGBRADSHAW@gmail.com>
2024-06-29 14:45:55 +0100

    first commit

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
-from eircode.address import Address
+
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-def search_address(address, reverse=True):
-  address = Address(address, proxy=False, reverse=reverse)
-  return address.serialize()
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
   if request.method == 'POST':
     address = request.form['address']
-    result = search_address(address)
-    print(result)
-    return render_template('index.html', result=result)
   return render_template('index.html', result=None)
 
 if __name__ == '__main__':
